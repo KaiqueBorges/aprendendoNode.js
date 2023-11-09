e0acb90cebc988f4ee5ca931f88c88c0bb2313d8
KaiqueBorges <kaiqueborges_255@hotmail.com>
2023-11-09 16:58:40 -0300

    projeto criado

---
+++ src/aprendendoNode.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const router = express.Router();
+
+router.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, + '/pages/home.html'));
+})
+
+router.get('/contato', (req, res) => {
+  res.sendFile(path.join(__dirname, + '/pages/home.html'));
+})
+
+app.use(router);
+app.listen(3333, () => {
+  console.log('SERVIDOR RODANDO');
+})
